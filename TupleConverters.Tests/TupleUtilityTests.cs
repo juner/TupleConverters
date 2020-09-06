7287581ca1cfb1653e6a9ce55bc2278f602addfb
juner <juner@juner.net>
2020-09-07 05:30:56 +0900

    実装とテストの追加

---
+++ TupleConverters.Tests/TupleUtilityTests.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using static TupleConverters.TupleUtility;
+
+namespace TupleConverters.Tests
+{
+    [TestClass()]
+    public class TupleUtilityTests
+    {
+        static IEnumerable<object?[]> ToTestData
+        {
+            get
+            {
+                // ValueTuple pattern 1-7
+                yield return ToTest(typeof((int, string)), new object?[] { 1, "2" }, (1, "2"));
+                // nested ValueTuple pattern 8 -
+                yield return ToTest(typeof((int, string, int, string, int, string, int, string, int, string)), new object?[] { 1, "2", 3, "4", 5, "6", 7, "8", 9, "10" }, (1, "2", 3, "4", 5, "6", 7, "8", 9, "10"));
+                // Tuple pattern 1-7
+                yield return ToTest(typeof(Tuple<int, string>), new object?[] { 1, "2" }, Tuple.Create(1, "2"));
+                // nested Tuple pattern 8 -
+                yield return ToTest(typeof(Tuple<int, string, int, string, int, string, int, Tuple<string, int, string>>), new object?[] { 1, "2", 3, "4", 5, "6", 7, "8", 9, "10" }, new Tuple<int, string, int, string, int, string, int, Tuple<string, int, string>>(1, "2", 3, "4", 5, "6", 7, new Tuple<string, int, string>("8", 9, "10")));
+                static object?[] ToTest(Type TupleType, IEnumerable<object?> Values, object Expected)
+                    => new object?[] { TupleType, Values, Expected };
+            }
+        }
+        [TestMethod]
+        [DynamicData(nameof(ToTestData))]
+        public void ToTest(Type TupleType, IEnumerable<object?> Values, object Expected)
+             => Assert.AreEqual(Expected, To(TupleType, Values));
+        static IEnumerable<object?[]> MakeTupleTypeTestData
+        {
+            get
+            {
+                yield return MakeTupleTypeTest(new Type[] { typeof(int), typeof(string) }, typeof(Tuple<int, string>));
+                
+                static object?[] MakeTupleTypeTest(IEnumerable<Type> Types, Type Expected)
+                    => new object?[] { Types, Expected };
+            }
+        }
+        [TestMethod]
+        [DynamicData(nameof(MakeTupleTypeTestData))]
+        public void MakeTupleTypeTest(IEnumerable<Type> Types, Type Expected)
+            => Assert.AreEqual(Expected, MakeTupleType(Types));
+
+        static IEnumerable<object?[]> MakeValueTupleTypeTestData
+        {
+            get
+            {
+                // ValueTuple pattern 1-7
+                yield return MakeValueTupleTypeTest(new Type[] { typeof(int), typeof(string) }, typeof((int, string)));
+                // nested ValueTuple pattern 8 -
+                yield return MakeValueTupleTypeTest(new Type[] { typeof(int), typeof(string), typeof(int), typeof(string), typeof(int), typeof(string), typeof(int), typeof(string), typeof(int), typeof(string) }, typeof((int, string,int, string,int, string,int, string,int, string)));
+                static object?[] MakeValueTupleTypeTest(IEnumerable<Type> Types, Type Expected)
+                    => new object?[] { Types, Expected };
+            }
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(MakeValueTupleTypeTestData))]
+        public void MakeValueTupleTypeTest(IEnumerable<Type> Types, Type Expected)
+            => Assert.AreEqual(Expected, MakeValueTupleType(Types));
+
+        static IEnumerable<object?[]> IsTupleTestData
+        {
+            get
+            {
+                yield return IsTupleTest(typeof((int, string)), false);
+                yield return IsTupleTest(typeof(Tuple<int, string>), true);
+                static object?[] IsTupleTest(Type Type, bool Expected)
+                    => new object?[] { Type, Expected };
+            }
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(IsTupleTestData))]
+        public void IsTupleTest(Type Type, bool Expected)
+            => Assert.AreEqual(Expected, Type.IsTuple());
+
+        static IEnumerable<object?[]> IsValueTupleTestData
+        {
+            get
+            {
+                yield return IsTupleTest(typeof((int, string)), true);
+                yield return IsTupleTest(typeof(Tuple<int, string>), false);
+                static object?[] IsTupleTest(Type Type, bool Expected)
+                    => new object?[] { Type, Expected };
+            }
+        }
+        [TestMethod]
+        [DynamicData(nameof(IsValueTupleTestData))]
+        public void IsValueTupleTest(Type Type, bool Expected)
+            => Assert.AreEqual(Expected, Type.IsValueTuple());
+
+        static IEnumerable<object?[]> FromTestData
+        {
+            get
+            {
+                yield return FromTest((1, "2"), new object?[] { 1, "2" });
+                yield return FromTest(Tuple.Create(1, "2"), new object?[] { 1, "2" });
+                static object?[] FromTest(object? Source, object?[] Expected)
+                    => new object?[] { Source, Expected };
+            }
+        }
+        [TestMethod]
+        [DynamicData(nameof(FromTestData))]
+        public void FromTest(object? Source, object?[] Expected)
+            => CollectionAssert.AreEqual(Expected, From(Source));
+    }
+}
---
+++ TupleConverters/TupleUtility.cs
@@ -0,0 +1,288 @@
+﻿#if NET45 || NET461 || NET47 || NETSTANDARD2_0
+#define ITUPLE_NOTSUPPORT
+#endif
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#if !ITUPLE_NOTSUPPORT
+using System.Runtime.CompilerServices;
+#endif
+namespace TupleConverters
+{
+    public static class TupleUtility
+    {
+        /// <summary>
+        /// オブジェクトの配列を元に規定の型に変換する
+        /// </summary>
+        /// <typeparam name="Tuple"></typeparam>
+        /// <param name="Value"></param>
+        /// <returns></returns>
+        public static
+#if ITUPLE_NOTSUPPORT
+            object
+#else
+            Tuple
+#endif
+            To<Tuple>(params object?[] Value)
+            where Tuple : class
+#if !ITUPLE_NOTSUPPORT
+            , ITuple
+#endif
+        {
+            return (Tuple)To(typeof(Tuple), Value);
+        }
+        public static object To(Type TupleType, IEnumerable<object?> Values, bool NoThrow = false)
+        {
+            var GenericArguments = TupleType.GetGenericArguments();
+            var Constructor = TupleType.GetConstructor(GenericArguments)!;
+            var Parameter = Values.Take(7);
+            if (GenericArguments.Length == 8)
+                Parameter = Parameter.Append(To(GenericArguments[7], Values.Skip(7)));
+            else if (!NoThrow && GenericArguments.Length != (Values is object?[] _Values ? _Values.Length : Values.Count()))
+                throw new ArgumentException($"Type Argument length:{GenericArguments.Length} is not Value length:{Values.Count()}", nameof(Values));
+            return
+#if ITUPLE_NOTSUPPORT
+#else
+            (ITuple)
+#endif
+            Constructor.Invoke(Parameter.ToArray());
+        }
+        public static Type MakeTupleType(IEnumerable<Type> Types) => (Types is Type[] __Types ? __Types.Length : Types.Count()) switch
+        {
+            0 => throw new ArgumentOutOfRangeException(nameof(Types) + " is 1 or later length support.", nameof(Types)),
+            1 => typeof(Tuple<>).MakeGenericType(Types is Type[] _Types ? _Types : Types.ToArray()),
+            2 => typeof(Tuple<,>).MakeGenericType(Types is Type[] _Types ? _Types : Types.ToArray()),
+            3 => typeof(Tuple<,,>).MakeGenericType(Types is Type[] _Types ? _Types : Types.ToArray()),
+            4 => typeof(Tuple<,,,>).MakeGenericType(Types is Type[] _Types ? _Types : Types.ToArray()),
+            5 => typeof(Tuple<,,,,>).MakeGenericType(Types is Type[] _Types ? _Types : Types.ToArray()),
+            6 => typeof(Tuple<,,,,,>).MakeGenericType(Types is Type[] _Types ? _Types : Types.ToArray()),
+            7 => typeof(Tuple<,,,,,,>).MakeGenericType(Types is Type[] _Types ? _Types : Types.ToArray()),
+            _ => typeof(Tuple<,,,,,,,>).MakeGenericType(Types.Take(7).Append(MakeTupleType(Types.Skip(7))).ToArray()),
+        };
+        public static Type MakeValueTupleType(IEnumerable<Type> Types) => (Types is Type[] __Types ? __Types.Length : Types.Count()) switch
+        {
+            0 => throw new ArgumentOutOfRangeException(nameof(Types) + " is 1 or later length support.", nameof(Types)),
+            1 => typeof(ValueTuple<>).MakeGenericType(Types is Type[] _Types ? _Types : Types.ToArray()),
+            2 => typeof(ValueTuple<,>).MakeGenericType(Types is Type[] _Types ? _Types : Types.ToArray()),
+            3 => typeof(ValueTuple<,,>).MakeGenericType(Types is Type[] _Types ? _Types : Types.ToArray()),
+            4 => typeof(ValueTuple<,,,>).MakeGenericType(Types is Type[] _Types ? _Types : Types.ToArray()),
+            5 => typeof(ValueTuple<,,,,>).MakeGenericType(Types is Type[] _Types ? _Types : Types.ToArray()),
+            6 => typeof(ValueTuple<,,,,,>).MakeGenericType(Types is Type[] _Types ? _Types : Types.ToArray()),
+            7 => typeof(ValueTuple<,,,,,,>).MakeGenericType(Types is Type[] _Types ? _Types : Types.ToArray()),
+            _ => typeof(ValueTuple<,,,,,,,>).MakeGenericType(Types.Take(7).Append(MakeValueTupleType(Types.Skip(7))).ToArray()),
+        };
+        public static bool IsTuple(this Type Tuple)
+        {
+            if (!Tuple.IsGenericType)
+                return false;
+            if (!Tuple.IsClass)
+                return false;
+#if ITUPLE_NOTSUPPORT
+            var OpenType = Tuple.GetGenericTypeDefinition();
+            return OpenType == typeof(Tuple<>)
+                || OpenType == typeof(Tuple<,>)
+                || OpenType == typeof(Tuple<,,>)
+                || OpenType == typeof(Tuple<,,,>)
+                || OpenType == typeof(Tuple<,,,,>)
+                || OpenType == typeof(Tuple<,,,,,>)
+                || OpenType == typeof(Tuple<,,,,,,>)
+                || (OpenType == typeof(Tuple<,,,,,,,>) && IsTuple(Tuple.GetGenericArguments()[7]));
+#else
+            return typeof(ITuple).IsAssignableFrom(Tuple);
+#endif
+        }
+        public static bool IsValueTuple(this Type Tuple)
+        {
+            if (!Tuple.IsGenericType)
+                return false;
+            if (!Tuple.IsValueType)
+                return false;
+#if ITUPLE_NOTSUPPORT
+            var OpenType = Tuple.GetGenericTypeDefinition();
+            return OpenType == typeof(ValueTuple<>)
+                || OpenType == typeof(ValueTuple<,>)
+                || OpenType == typeof(ValueTuple<,,>)
+                || OpenType == typeof(ValueTuple<,,,>)
+                || OpenType == typeof(ValueTuple<,,,,>)
+                || OpenType == typeof(ValueTuple<,,,,,>)
+                || OpenType == typeof(ValueTuple<,,,,,,>)
+                || (OpenType == typeof(ValueTuple<,,,,,,,>) && IsTuple(Tuple.GetGenericArguments()[7]));
+#else
+            return typeof(ITuple).IsAssignableFrom(Tuple);
+#endif
+        }
+        public static object?[] From(object? Source) => Source.GetEnumerable().ToArray();
+        internal static IEnumerable<object?> GetEnumerable(this object? Source)
+        {
+            if (Source is null)
+                throw new ArgumentNullException($"{nameof(Source)} is null", nameof(Source));
+
+            var Type = Source.GetType();
+
+#if ITUPLE_NOTSUPPORT
+            if (Type.IsValueType)
+                return Source.GetValueTupleEnumerable();
+            if (Type.IsClass)
+                return Source.GetTupleEnumerable();
+#else
+            if (Source is ITuple Tuple)
+                return Tuple.GetEnumerable();
+#endif
+            throw new NotSupportedException($"{nameof(Source)} is not Tuple Type ( Tuple<> or ValueTuple<>)");
+        }
+#if ITUPLE_NOTSUPPORT
+        internal static IEnumerable<object?> GetValueTupleEnumerable(this object? Values)
+        {
+            if (Values is null)
+                throw new ArgumentNullException($"{nameof(Values)} is null", nameof(Values));
+            return GetEnumerable(Values.GetType(), Values);
+            static IEnumerable<object?> GetEnumerable(Type Type, object? Source)
+            {
+                var GenericArguments = Type.GetGenericArguments().Length;
+                if (GenericArguments == 1)
+                {
+                    yield return Type.GetField(nameof(ValueTuple<int>.Item1)).GetValue(Source);
+                }
+                else if (GenericArguments == 2)
+                {
+                    yield return Type.GetField(nameof(ValueTuple<int, int>.Item1)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int>.Item2)).GetValue(Source);
+                }
+                else if (GenericArguments == 3)
+                {
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int>.Item1)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int>.Item2)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int>.Item3)).GetValue(Source);
+                }
+                else if (GenericArguments == 4)
+                {
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int>.Item1)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int>.Item2)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int>.Item3)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int>.Item4)).GetValue(Source);
+                }
+                else if (GenericArguments == 5)
+                {
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int>.Item1)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int>.Item2)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int>.Item3)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int>.Item4)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int>.Item5)).GetValue(Source);
+                }
+                else if (GenericArguments == 6)
+                {
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int>.Item1)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int>.Item2)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int>.Item3)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int>.Item4)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int>.Item5)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int>.Item6)).GetValue(Source);
+                }
+                else if (GenericArguments == 7)
+                {
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int>.Item1)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int>.Item2)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int>.Item3)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int>.Item4)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int>.Item5)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int>.Item6)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int>.Item7)).GetValue(Source);
+                }
+                else if (GenericArguments == 8)
+                {
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int, int>.Item1)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int, int>.Item2)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int, int>.Item3)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int, int>.Item4)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int, int>.Item5)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int, int>.Item6)).GetValue(Source);
+                    yield return Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int, int>.Item7)).GetValue(Source);
+                    var Rest = Type.GetField(nameof(ValueTuple<int, int, int, int, int, int, int, int>.Rest)).GetValue(Source);
+                    foreach (var r in GetEnumerable(Rest.GetType(), Rest))
+                        yield return r;
+                }
+                yield break;
+            }
+        }
+        internal static IEnumerable<object?> GetTupleEnumerable(this object? Values)
+        {
+            if (Values is null)
+                throw new ArgumentNullException($"{nameof(Values)} is null", nameof(Values));
+            return GetEnumerable(Values.GetType(), Values);
+            static IEnumerable<object?> GetEnumerable(Type Type, object? Source)
+            {
+                var GenericArguments = Type.GetGenericArguments().Length;
+                if (GenericArguments == 1)
+                {
+                    yield return Type.GetProperty(nameof(Tuple<int>.Item1)).GetValue(Source);
+                }
+                else if (GenericArguments == 2)
+                {
+                    yield return Type.GetProperty(nameof(Tuple<int, int>.Item1)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int>.Item2)).GetValue(Source);
+                }
+                else if (GenericArguments == 3)
+                {
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int>.Item1)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int>.Item2)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int>.Item3)).GetValue(Source);
+                }
+                else if (GenericArguments == 4)
+                {
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int>.Item1)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int>.Item2)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int>.Item3)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int>.Item4)).GetValue(Source);
+                }
+                else if (GenericArguments == 5)
+                {
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int>.Item1)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int>.Item2)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int>.Item3)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int>.Item4)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int>.Item5)).GetValue(Source);
+                }
+                else if (GenericArguments == 6)
+                {
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int>.Item1)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int>.Item2)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int>.Item3)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int>.Item4)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int>.Item5)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int>.Item6)).GetValue(Source);
+                }
+                else if (GenericArguments == 7)
+                {
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int>.Item1)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int>.Item2)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int>.Item3)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int>.Item4)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int>.Item5)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int>.Item6)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int>.Item7)).GetValue(Source);
+                }
+                else if (GenericArguments == 8)
+                {
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int, int>.Item1)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int, int>.Item2)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int, int>.Item3)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int, int>.Item4)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int, int>.Item5)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int, int>.Item6)).GetValue(Source);
+                    yield return Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int, int>.Item7)).GetValue(Source);
+                    var Rest = Type.GetProperty(nameof(Tuple<int, int, int, int, int, int, int, int>.Rest)).GetValue(Source);
+                    foreach (var r in GetEnumerable(Rest.GetType(), Rest))
+                        yield return r;
+                }
+                yield break;
+            }
+        }
+#else
+        internal static IEnumerable<object?> GetEnumerable(this ITuple Source)
+        {
+            for (var i = 0; i < Source.Length; i++)
+                yield return Source[i];
+        }
+#endif
+    }
+}
